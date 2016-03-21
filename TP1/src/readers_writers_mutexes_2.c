290eaf797fa6e852ef9daed6b0dc7c79a9f32217
Xuecan Yang <xuecan.yang@telecom-paristech.fr>
2016-03-21 22:50:21 +0100

    wip 1.5

--- TP1/src/readers_writers_mutexes_2.c
+++ TP1/src/readers_writers_mutexes_2.c
@@ -14,9 +14,15 @@ void rw_mutex_init (rw_mutex_t * rw_mutex, int n_threads){
   UNUSED = (void *) &UNUSED_K;
   rw_mutex->n_readers = 0;
   rw_mutex->owner = UNUSED;
+  rw_mutex->queue = circular_buffer_init(20);
+  pthread_mutex_init(&(rw_mutex->mutex),NULL);
+  pthread_cond_init(&(rw_mutex->guard),NULL);
 }
 
 void rw_mutex_read_lock (rw_mutex_t *rw_mutex, thread_conf_t *conf){
+  pthread_mutex_lock(&(rw_mutex->mutex));
+  while()
+      pthread_cond_wait(&(rw_mutex->guard),&(rw_mutex->mutex));
   int i;
   rw_mutex->owner = READER;
   for (i=0; i < rw_mutex->n_readers; i++) printf ("  ");
@@ -30,12 +36,16 @@ void rw_mutex_read_unlock (rw_mutex_t *rw_mutex, thread_conf_t * conf){
   printf ("reader (%ld) : leave r = %d v = %ld\n",
 	  conf->identifier, rw_mutex->n_readers, shared_variable);
   rw_mutex->owner = UNUSED;
+  pthread_mutex_unlock(&(rw_mutex->mutex));
 }
 
 void rw_mutex_writer_lock (rw_mutex_t *rw_mutex, thread_conf_t * conf){
+  pthread_mutex_lock(&(rw_mutex->mutex));
   rw_mutex->owner = WRITER;
 }
 
 void rw_mutex_writer_unlock (rw_mutex_t *rw_mutex, thread_conf_t * conf){
   rw_mutex->owner = UNUSED;
+  pthread_cond_broadcast(&(rw_mutex->guard));
+  pthread_mutex_unlock(&(rw_mutex->mutex));
 }
