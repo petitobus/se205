0d1ba3a9c05c79ad7399ccfdf42de358d12365db
Xuecan YANG <xuecan.yang@telecom-paristech.fr>
2016-03-21 02:20:58 +0100

    erreur de compilation

--- TP1/src/main_join.c
+++ TP1/src/main_join.c
@@ -10,22 +10,25 @@
 int last_thread = -1;
 int first_thread = 0;
 pthread_t threads_table[MAXTHREADS];
-
-
+struct timeval s = {0, 0};
+pthread_cond_t cond;
+pthread_mutex_t mtx;
 // Terminate thread
 void process_exit() {
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  printf("thread (%x) join after %d s\n", pthread_self(), (int)(t.tv_sec -s.tv_sec));
+  pthread_cond_broadcast(&cond);
   pthread_exit(NULL);
 }
 
 // Find a terminated child process and return its tid
 pthread_t process_wait() {
-  pthread_t thread = NULL;
-  
+    pthread_t thread = NULL;
   if (first_thread <= last_thread) {
     thread = threads_table[first_thread];
     first_thread++;
   }
-
   return thread;
 }
 
@@ -40,14 +43,12 @@ void thread_main(void){
 }
 
 int main(int argc, char *argv[]){
-  struct timeval t;
-  struct timeval s = {0, 0};
-  
   if (argc != 2){ 
     printf("Usage : %s <n_threads>\n", argv[0]);
     exit(1);
   } 
-
+  pthread_cond_init(&cond,NULL);
+  pthread_mutex_init(&mtx,NULL);
   srandom(time(NULL));
   gettimeofday(&s, NULL);
   last_thread = atoi(argv[1]) - 1;
@@ -60,12 +61,23 @@ int main(int argc, char *argv[]){
   }
 
   // Attendre la terminaison des threads dans un certain ordre
+  int conter = last_thread;
+  while(conter != -1){
+ 	 pthread_mutex_lock(&mtx);
+ 	 pthread_cond_wait(&cond,&mtx);
+	 conter--;
+ 	 pthread_mutex_unlock(&mtx);
+  }
+    printf("all threads finish");
+ /* 
   pthread_t thread;
   while ((thread = process_wait()) != NULL) {
     pthread_join(thread,NULL);
+	struct timeval t;
     gettimeofday(&t, NULL);
     printf("thread (%p) join after %d s\n", (void *)thread, (int)(t.tv_sec -s.tv_sec));
   }
+  */
   return 0;
 
 }
